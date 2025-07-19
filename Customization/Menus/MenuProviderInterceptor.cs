eec358f19e40407602383fa26d29b182d0872269
pawan <pawan@example.com>
2025-07-20 00:19:55 +0530

    .

---
+++ Customization/Menus/MenuProviderInterceptor.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Shell.Navigation;
+
+namespace alloy_example.Customization.Menus;
+
+public class MenuProviderInterceptor : IMenuProvider
+{
+    private readonly IMenuProvider _menu;
+
+    public MenuProviderInterceptor(IMenuProvider menu)
+    {
+        _menu = menu;
+    }
+
+    public IEnumerable<MenuItem> GetMenuItems()
+    {
+        var menuItems = _menu.GetMenuItems().ToList();
+        return menuItems;
+    }
+}
